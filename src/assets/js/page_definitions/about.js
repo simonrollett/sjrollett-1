5d4d02a186f68ee4f0320808c112b0d6a237447f
rollett <srollett@qmetric.co.uk>
2019-12-08 11:20:33 +0000

    restructure code

---
+++ src/assets/js/page_definitions/about.js
@@ -0,0 +1,26 @@
+const page_content = {
+  config: {
+    html_template: "template_home"
+  },
+    head:{
+        title: "about title here"
+    },
+    content:{
+        settings:{
+            preheader: '',
+            login: true,
+            help: true,
+            tel: false
+        },
+        head:[],
+        body:[
+            "About"
+        ],
+        footer:[
+
+        ],
+        terms:[]
+    }
+};
+
+module.exports = page_content;
---
+++ src/assets/js/page_templates/template_home.js
@@ -0,0 +1,20 @@
+function template_home(data){
+return `<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+
+<head>
+  <title> ${data.head.title}</title>
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700" rel="stylesheet" type="text/css">
+  <style type="text/css">
+    @import url(https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700);
+  </style>
+</head>
+
+<body>
+  <div style="">
+  </div>
+</body>
+
+</html>
+`}
+module.exports = template_home;
